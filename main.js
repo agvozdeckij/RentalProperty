61957b78d57142dc94e796d27fb80b51534e4180
Artem Hvozdetskyi <agaozdeckij@gmail.com>
2022-03-20 00:06:04 +0200

    update js

--- main.js
+++ main.js
@@ -2,11 +2,18 @@ const sidebarToggelBtn = document.querySelector('.menu-icon-wrapper');
 const menuIcon = document.querySelector('.menu-icon');
 const sideBar = document.querySelector('.sidebar');
 
-
-
-
-
 sidebarToggelBtn.onclick = function () {
     menuIcon.classList.toggle('menu-icon-active');
     sideBar.classList.toggle('sidebar--mobile--active');
 }
+
+
+const btnShowMoreCards = document.querySelector('.btn');
+const hiddenCards = document.querySelectorAll('.card-link-hidden');
+
+btnShowMoreCards.addEventListener('click', function () {
+    hiddenCards.forEach(function (card){
+        card.classList.remove('card-link-hidden');
+    })
+
+})
